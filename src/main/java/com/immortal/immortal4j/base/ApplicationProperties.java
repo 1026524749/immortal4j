452141f5a1c9d91fb17ac96f6887cce0cba48797
shijieming <jie8309541@qq.com>
2018-10-06 03:31:57 +0800

    更新最新的测试代码

--- src/main/java/com/immortal/immortal4j/base/ApplicationProperties.java
+++ src/main/java/com/immortal/immortal4j/base/ApplicationProperties.java
@@ -9,25 +9,6 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class ApplicationProperties {
-    @Value("${com.neo.title}")
-    private String title;
 
-    @Value("${com.neo.description}")
-    private String description;
 
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
